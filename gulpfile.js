7dc85449994657b9f09c0079c28f185ba25b0411
Fernando Nantes-Machado <fernandonantesmachadoytoledo@gmail.com>
2017-04-19 11:29:45 +0100

    Modificado el gulpfile para realizar tests con las clases rescritas.

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,8 @@ var shell = require('gulp-shell');
 
 gulp.task("default", ["test"]);
 
-gulp.task('debug', shell.task('node --inspect --debug-brk index.js'));
+gulp.task('debug', shell.task('node --inspect --debug-brk ecma6/index.js'));
 
-gulp.task('run', shell.task('node index.js'));
+gulp.task('run', shell.task('node ecma6/index.js'));
 
-gulp.task("test", shell.task("./node_modules/mocha/bin/mocha --require should"));
+gulp.task("test", shell.task("./node_modules/mocha/bin/mocha --require should ecma6/test_ecma6/test.js"));
