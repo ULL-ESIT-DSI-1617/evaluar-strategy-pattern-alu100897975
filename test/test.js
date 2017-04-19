461804bb2abd5ca0572840fc9323403e2f6a2af2
Fernando Nantes-Machado <fernandonantesmachadoytoledo@gmail.com>
2017-04-19 11:46:48 +0100

    Pruebas para travis añadidas

--- test/test.js
+++ test/test.js
@@ -1,9 +1,27 @@
-var getArea = require("../shapesarea.js");
+var Triangle = require('../triangle.js');
+var Square = require('../square.js');
+var Rectangle = require('../rectangle.js');
 
-describe("getArea", function() {
-  it("must compute the triangle area correctly", function() {
-    var result = getArea('Triangle',  { width: 100, height: 100 });
-    /* There is a white space between consecutive columns */
-    result.should.match(/^5000$/);
+describe("Pruebas de Triángulo", function(){
+  it("Se calcula correctamente el área", function(){
+    var forma = new Triangle({width: 10, height: 5});
+    var area = forma.getArea();
+    area.should.match(/^25$/);
   })
-});
+})
+
+describe("Pruebas de Rectángulo", function(){
+  it("Se calcula correctamente el área", function(){
+    var forma = new Rectangle({width: 10, height: 5});
+    var area = forma.getArea();
+    area.should.match(/^50$/);
+  })
+})
+
+describe("Pruebas de Cuadrado", function(){
+  it("Se calcula correctamente el área", function(){
+    var forma = new Square({width: 10});
+    var area = forma.getArea();
+    area.should.match(/^100$/);
+  })
+})
